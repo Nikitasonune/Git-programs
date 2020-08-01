828a050bda723d592a35ee986d5e7c8f7bf3395f
Nikitasonune <nikitasonune@gmail.com>
2020-08-01 10:57:23 -0700

    1D Array

---
+++ 1-July-2020/1DArrayStore subject marks/1DArrayStore subject marks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1DArrayStore_subject_marks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] marks = new int[5];
+            int i;
+            for (i = 0; i <= 4; i++) 
+            {
+                Console.WriteLine("Enter marks: ");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //displaying the total marks
+            int total = 0;
+            foreach(int m in marks)
+            {
+                total = total + m;
+                Console.WriteLine("Total marks is: " + m);   
+            }
+            Console.WriteLine("Total marks is: " + total);
+            Console.ReadKey();
+        }
+    }
+}
