50396dbb95cb6cbedc2c11867d5d217ab4e90733
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:51:42 -0700

    Geometric series

---
+++ 3_march/2_GeometricSeries/series2_roshanSir/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace series2_roshanSir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.Geometric series
+            int i = 5, num;
+             Console.WriteLine("Enter number you want: ");
+             num = int.Parse(Console.ReadLine());
+             for(i=5;i<= num;i=i*2)
+             {
+
+                 Console.WriteLine(i);
+
+             }
+        }
+    }
+}
