9f2fd9b6478e005748a8ccaf37f36b7421d7d3ce
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:53:40 -0700

    Triangular series

---
+++ 3_march/5_TriangularSeries/5_TriangularSeries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_TriangularSeries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.Triangular series
+             int i, num,sum=0;
+             Console.WriteLine("Enter number you want: ");
+             num = int.Parse(Console.ReadLine());
+             for (i = 1; i <= num; i++)
+             {
+                 sum = sum + i;
+                 Console.WriteLine(sum);
+             }
+        }
+    }
+}
