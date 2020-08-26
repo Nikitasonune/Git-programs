dc66a8809107851497ba6583abcaaafdb42c9e9a
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:46:35 -0700

    Practice Assignment

---
+++ 2_march(method,classes,loop)/roshSirAssign2/roshSirAssign2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roshSirAssign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.
+            float s = 0, j = 2, i=3;
+            s = s + 1;
+            while(i<=8)
+             {
+
+                 s = s + (i / j);
+                 i = i + 2;
+                 j = j * 2;
+
+             }
+
+             Console.WriteLine("Result is: " + s);
+             Console.ReadKey();
+        }
+    }
+}
