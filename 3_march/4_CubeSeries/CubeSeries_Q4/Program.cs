6335150c8909a38718ea25ad44ccd7ece18ff090
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:52:57 -0700

    Cube Series

---
+++ 3_march/4_CubeSeries/CubeSeries_Q4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CubeSeries_Q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.cube series
+             int i = 1, num;
+             Console.WriteLine("Enter number you want: ");
+             num = int.Parse(Console.ReadLine());
+             for (i = 1; i <= num; i++)
+             {
+                 Console.WriteLine(i * i*i);
+             }
+        }
+    }
+}
