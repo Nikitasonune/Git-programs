dbb556fc13da14878e547affe53eaf691b149a22
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:54:58 -0700

    Inheritance Assignment 2

---
+++ 3_march/Inheritance_Assign_2/Inheritance_Assign_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Assign_2
+{
+    //2.You Just Only need to check error
+    class Parent
+    {
+        private void showdata()
+        {
+            Console.WriteLine("This is parent class");
+        }
+    }
+    class child : Parent
+    {
+        public void showdata2()
+        {
+            Console.WriteLine("This is child class");
+        }
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            p.showdata();
+            child c = new child();
+            c.showdata2();
+            c.showdata();
+            Console.ReadKey();
+
+        }
+    }
+
+}
