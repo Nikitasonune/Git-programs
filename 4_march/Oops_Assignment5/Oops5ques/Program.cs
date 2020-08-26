e6acb8df25791338759769de5c553e1511c36e12
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 13:01:01 -0700

    OOPs Assignment

---
+++ 4_march/Oops_Assignment5/Oops5ques/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops5ques
+{
+    //5.
+    class Rectangle
+    {
+        public int length;
+        public int breadth;
+        
+        public Rectangle(int len, int brd)
+        {
+            length = len;
+            breadth = brd;
+        }
+        public string area()
+        {
+            int Area = length * breadth;
+            Console.WriteLine("area of Rectangle is: " + Area);
+            return "area of Rectangle is " + Area + " of length and breadth: " + length + breadth;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Rectangle r = new Rectangle(4, 5);
+            r.area();
+            Rectangle s = new Rectangle(5, 8);
+            s.area();
+            Console.ReadKey();
+        }
+    }
+}
