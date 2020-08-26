8dd099c9265020e56eb76ad7e0b1231b73100afc
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:27:22 -0700

    Polymorphism Assignment 2

---
+++ 3_march/polymorphism_Assignment1/polymorphism/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism
+{
+    //creating base class
+    class Figure
+    {
+        public void draw()
+        {
+            Console.WriteLine("Coiding of figure class draw method");
+        }
+    }
+    class Rectangle : Figure
+    {
+        //overriding the base class inherited draw method
+        public void draw()
+        {
+            Console.WriteLine("Coiding of Rectangle class draw method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure r = new Rectangle();
+            r.draw();
+            Console.ReadKey();
+        }
+    }
+}
