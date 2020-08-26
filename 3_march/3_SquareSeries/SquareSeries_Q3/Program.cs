63ad6fd5394e810a6590c2b6807ccd3a99f2cda7
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:52:22 -0700

    Square series

---
+++ 3_march/3_SquareSeries/SquareSeries_Q3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareSeries_Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.square series
+            int i = 1, num;
+            Console.WriteLine("Enter number you want: ");
+            num = int.Parse(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                Console.WriteLine(i*i);
+            }
+        }
+    }
+}
