cd4bcd95f06d8a6109ac1587c50c5a7af796163c
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:08:06 -0700

    Inheritance Assignment 4

---
+++ 3_march/Inheritance_Assign_4/Inheritance_Assign_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_Assign_4
+{
+    //4.
+    class Rectangle
+     {
+         public int length;
+         public int breadth;
+         public Rectangle()
+         {
+             Console.WriteLine("default constructor");
+         }
+         public Rectangle(int len,int brd)
+         {
+             length=len;
+             breadth=brd;
+         }
+         public void area()
+         {
+             int Area = length * breadth;
+             Console.WriteLine("Area of Rectangle is: " + Area);
+         }
+         public void Perimeter()
+         {
+             int Perimeter = length + breadth;
+             Console.WriteLine("Area of Rectangle is: " + Perimeter);
+         }
+     }
+     class Square : Rectangle
+     {
+         public int side;
+         public Square(int sq)
+         {
+             side=sq;
+
+         }
+         public void area()
+         {
+             int Area = side * side;
+             Console.WriteLine("Area of Square is: " + Area);
+         }
+         public void Perimeter()
+         {
+             float Perimeter = 4 * side;
+             Console.WriteLine("Area of Square is: " + Perimeter);
+         }
+     }
+     class Program
+     {
+
+         static void Main(string[] args)
+         {
+             Rectangle R = new Rectangle(5,10);
+             R.area();
+             R.Perimeter();
+             Square S = new Square(10);
+             S.area();
+             S.Perimeter();
+             Console.ReadKey();
+         }
+     }
+}
