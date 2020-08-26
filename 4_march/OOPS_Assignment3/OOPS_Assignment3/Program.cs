8ca8dd0cb159b886172644f46c00a1a1819086a9
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:57:46 -0700

    OOPs 3

---
+++ 4_march/OOPS_Assignment3/OOPS_Assignment3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOPS_Assignment3
+{
+    //3.
+     class Triangle
+     {
+         public int side1 = 3;
+         public int side2 = 4;
+         public int side3=5; 
+         //constructor
+
+         public Triangle()
+         {
+
+             Console.WriteLine("Area of Triangle is");
+             float Area = side1 * side2/2;
+             Console.WriteLine("Area of Triangle: " + Area);
+
+             Console.WriteLine("Perimeter of triangle is");
+             int Perimeter = side1 + side2 + side3;
+             Console.WriteLine("Perimeter of triangle is: " + Perimeter);
+         }
+     }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             Triangle obj = new Triangle();
+             Console.ReadKey();
+         }
+     }
+
+
+
+}
