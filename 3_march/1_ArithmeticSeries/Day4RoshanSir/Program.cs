71eac9e216a99d89d4a26b84468ab5602a2ab80c
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:50:55 -0700

    Arithmetic series

---
+++ 3_march/1_ArithmeticSeries/Day4RoshanSir/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4RoshanSir
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //1.Arithmetic series 1,4,7,10,13,16,19......n
+            int i = 1,num;
+            Console.WriteLine("Enter number you want: ");
+            num = int.Parse(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+
+                Console.WriteLine(i);
+                i = i + 2;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
