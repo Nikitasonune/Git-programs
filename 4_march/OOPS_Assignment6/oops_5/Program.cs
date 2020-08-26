a99d7e22b7fa549fd4f2b6c696925924a34d6b38
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:58:58 -0700

    OOPs 6

---
+++ 4_march/OOPS_Assignment6/oops_5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_5
+{
+    //6.
+    class Area
+    {
+        public int length;
+        public int breadth;
+        public int area=0;
+
+        public void setDim(int len,int brd)
+        {
+            length = len;
+            breadth = brd;
+        }
+        public int getArea()
+        {
+            area = length * breadth;
+            return area;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length and breadth of rectangle: ");
+            int len = int.Parse(Console.ReadLine());
+            int brd = int.Parse(Console.ReadLine());
+
+            Area obj = new Area();
+            obj.setDim(len,brd);
+            int res = obj.getArea();
+
+            Console.WriteLine("Area of Rectangle is: " + res);
+            Console.ReadKey();
+
+        }
+    }
+}
