007e6f0a32887fedda16fe93f28ad1f1c52d9a91
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:28:59 -0700

    Abstract Class Assignment 2

---
+++ 3_march/Abstract_Class_Assignment2/Abstract_Class_Assignment2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Abstract_Class_Assignment2
+{
+    //4.abstract class
+    abstract class Account
+    {
+        public abstract void Deposite (int amt);
+        public void showbalance()
+        {
+            Console.WriteLine("balance");
+        }
+    }
+    class Saving : Account
+    {
+        public override void Deposite(int amt)
+        {
+            Console.WriteLine("deposite "+amt);
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Account act = new Saving();
+            act.showbalance();
+            act.Deposite(2);
+            Console.ReadKey();
+
+        }
+    }
+
+
+}
