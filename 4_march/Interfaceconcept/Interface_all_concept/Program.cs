22451c9969a68a0899ce4e59442f63f099135f9a
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:51:05 -0700

    IntercaeConcept

---
+++ 4_march/Interfaceconcept/Interface_all_concept/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_all_concept
+{
+
+    //1.
+    class Token
+    {
+        protected Token(string name)
+        {
+            Console.WriteLine("base class constructor: name= " + name);
+        }
+    }
+    class commentToken : Token
+    {
+        public commentToken(string name): base(name)
+        {
+            Console.WriteLine("derived class constructor: name= " + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            commentToken ct = new commentToken("Nikita");
+            Console.ReadKey();
+        }
+    }
+
+
+
+}
+        
+
+        
+
+
+
+    
+
+
+
+
+
