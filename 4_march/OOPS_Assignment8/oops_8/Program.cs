cb6c2b229251cddd7b1a2e8b20f02b6720b11279
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:59:46 -0700

    OOPs 8

---
+++ 4_march/OOPS_Assignment8/oops_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_8
+{
+    class Average
+    {
+        public void calculate(int num1, int num2, int num3)
+        {
+            int average = (num1 + num2 + num3) / 3;
+            Console.WriteLine("Average of three numbers are " + average);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter number 3: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Average obj = new Average();
+            obj.calculate(num1, num2, num3);
+            Console.ReadKey();
+        }
+    }
+}
