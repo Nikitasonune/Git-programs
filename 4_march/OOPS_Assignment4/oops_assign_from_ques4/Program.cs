82869b8e633e0a02bbd89942ee348bdb62ea180d
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:58:25 -0700

    OOPS 4

---
+++ 4_march/OOPS_Assignment4/oops_assign_from_ques4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_assign_from_ques4
+{
+    //4.
+    class Triangle
+    {
+        //4.oops Assign
+        public int side1 { get; set; }
+        public int side2 { get; set; }
+        public int side3 { get; set; }
+       
+        //constructor
+
+        public Triangle(int a, int b, int c)
+        {
+            side1 = a;
+            side2 = b;
+            side3 = c;
+           
+            int  Area = (side1 * side2) / 2;
+            Console.WriteLine("Area of Triangle: " + Area);
+            Console.WriteLine();
+
+           
+           int Perimeter = side1 + side2 + side3;
+            Console.WriteLine("Perimeter of triangle is: " + Perimeter);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle obj = new Triangle(3, 4, 5);
+            Console.ReadKey();
+        }
+    }
+}
+    
+
