e3804fbd386c18ab492ed2938103eb8d460a9777
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:52:44 -0700

    Multiple Inheritance(Implicit Type)

---
+++ 4_march/Multiple_Interface/Multiple_Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Multiple_Interface
+{
+    //2.multiple interface (Implict Intreface)
+     interface IToken
+     {
+          string Name();
+     }
+     interface IVisitable
+     {
+          void Accept(string n);
+     }
+     class Token : IToken, IVisitable
+     {
+         public string Name()
+         {
+             return "Hello frome name";
+         }
+         public void Accept(string n)
+         {
+             Console.WriteLine("name= " + n);
+         }
+     }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             Token t = new Token();
+             Console.WriteLine(t.Name());
+             t.Accept("Nikita");
+             Console.ReadKey();
+         }
+     }
+
+}
