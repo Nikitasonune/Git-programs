85b53d6b977e2d89a2726af8548a6b8870dc0abf
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:25:56 -0700

    Inheritance Assignment 7

---
+++ 3_march/Inheritance_Assignment7/Inheritance_Assignment7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Inheritance_Assignment7
+{
+    //5.Interface
+    interface Account
+    {
+        void Deposit(int amt);
+    }
+    interface Account1 : Account
+    {
+        void showbalance();
+    }
+    class Saving : Account1
+    {
+        public void Deposit(int amt)
+        {
+            Console.WriteLine("deposite " + amt);
+        }
+        public void showbalance()
+        {
+            Console.WriteLine("Show");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Saving act = new Saving();
+            act.Deposit(2);
+            act.showbalance();
+            Console.ReadKey();
+
+        }
+    }
+}
