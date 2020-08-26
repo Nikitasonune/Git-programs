f8bad7e9ec5828b21c91e3af6efc88e30b60f266
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:57:01 -0700

    OOPS Assignment 2

---
+++ 4_march/OOPS_Assignment2/OOPS_Assignment2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOPS_Assignment2
+{
+    //2.
+    class Student
+    {
+        public int PhoneNumber { set; get; }
+        public int RollNumber { set; get; }
+        public string Address { set; get; }
+        public string Name;
+        public void getdata(string name,int rollNumber, int phoneNumber,string address)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            RollNumber = rollNumber;
+            Address = address;
+        }
+        public void setdata()
+        {
+            Console.WriteLine("Student Name is: " + Name);
+            Console.WriteLine("Student Phone number is: " + PhoneNumber);
+            Console.WriteLine("Student Roll number is: " + RollNumber);
+            Console.WriteLine("Student Address is: " + Address);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+
+            Console.WriteLine("Enter Roll number: ");
+            int rno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter phone number: ");
+            int pH = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter student Address: ");
+            string add = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Student 1 Detaile are: ");
+
+            s.getdata("John",rno,pH,add);
+            s.setdata();
+
+
+            Student obj = new Student();
+            Console.WriteLine("\nEnter Roll number: ");
+            int Rno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter phone number: ");
+            int phone = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter student Address: ");
+            string address = Console.ReadLine();
+            Console.WriteLine("\nStudent 2 Detaile are: ");
+            obj.getdata("Sam", Rno, phone, address);
+            obj.setdata();
+            Console.ReadKey();
+        }
+    }
+}
