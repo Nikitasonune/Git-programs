b1acd1a424a0d950bf5a3da5d26d6ad7971b3425
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 12:53:50 -0700

    Multiple Inheritance(Explicit Type)

---
+++ 4_march/Multiple_Interface(ExplicitType)/Multiple_Interface(ExplicitType)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Multiple_Interface_ExplicitType_
+{
+    //3.(Explicit Interface)
+     interface IToken
+     {
+         string Name();
+     }
+     interface IVisitable
+     {
+         void Accept(string n);
+     }
+     class Token : IToken, IVisitable
+     {
+          string IToken.Name()
+         {
+             return "Hello frome name";
+         }
+          void IVisitable.Accept(string n)
+         {
+             Console.WriteLine("name= " + n);
+         }
+     }
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             IToken t = new Token();
+             Console.WriteLine(t.Name());
+
+             IVisitable t1 = new Token();
+             t1.Accept("Nikita");
+             Console.ReadKey();
+         }
+     }
+
+}
