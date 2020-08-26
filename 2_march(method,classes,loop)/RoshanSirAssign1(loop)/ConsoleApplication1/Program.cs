ecc0790181984f7724eef0c8192d469856010f49
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:45:43 -0700

    Assignment on loop

---
+++ 2_march(method,classes,loop)/RoshanSirAssign1(loop)/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            float s =0,i;
+             for(i=1;i<=50;i++)
+              {
+                  s = s + 1 / i;
+              }
+              Console.WriteLine("Result is: " +s);
+            Console.ReadKey();
+
+        }
+    }
+}
