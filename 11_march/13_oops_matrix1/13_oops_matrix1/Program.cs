9edff81354e5a28b52cd4bfc1c22a18592b227ba
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 13:43:41 -0700

    Matrix Addittion

---
+++ 11_march/13_oops_matrix1/13_oops_matrix1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_oops_matrix1
+{
+    class Matrix
+    {
+        int Row { get; set; }
+        int Column { get; set; }
+        
+        public Matrix()
+        {
+            Console.WriteLine("Enter row of matrix: ");
+            int row = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter column of matrix: ");
+            int col = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[row, col];
+
+            Console.WriteLine("Enter matrix elements: ");
+            for(int i=0;i< row;i++)
+            {
+                for(int j=0;j< col;j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Printed matrix elements are: ");
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(matrix[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix obj = new Matrix();
+            Console.ReadKey();
+        }
+    }
+}
