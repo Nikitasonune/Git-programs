fc984f37d3c47c183062ac79753ecc092953246f
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 11:54:22 -0700

    Inheritance Assignment 1

---
+++ 3_march/Inheritance_Assign_1/Inheritance_assign_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_assign_3
+{
+    //1.
+    class Parent
+     {
+         public void showdata()
+         {
+             Console.WriteLine("This is parent class");
+         }
+     }
+     class child : Parent
+     {
+         public void showdata2()
+         {
+             Console.WriteLine("This is child class");
+         }
+
+     }
+     class Program
+     {
+
+         static void Main(string[] args)
+         {
+             Parent p = new Parent();
+             p.showdata();
+             child c = new child();
+             c.showdata2();
+             c.showdata();
+             Console.ReadKey();
+
+         }
+     }
+}
+
