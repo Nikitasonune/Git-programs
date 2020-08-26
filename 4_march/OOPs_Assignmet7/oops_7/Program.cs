3d9ae55bf9af57bbbc9bdc852d3f99ccb020bdd0
Nikitasonune <nikitasonune@gmail.com>
2020-08-26 13:00:22 -0700

    OOPS 7

---
+++ 4_march/OOPs_Assignmet7/oops_7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_7
+{
+        class Area
+        {
+            public int length;
+            public int breadth;
+            public int area;
+            
+           
+
+            public Area(int len, int brd)
+            {
+                length = len;
+                breadth = brd;
+            }
+            public int returnArea()
+            {
+                area = length * breadth;
+                return area;
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Enter length and breadth of rectangle: ");
+                int len = int.Parse(Console.ReadLine());
+                int brd = int.Parse(Console.ReadLine());
+
+                Area obj = new Area(len,brd);
+               
+                int res = obj.returnArea();
+
+                Console.WriteLine("Area of Rectangle is: " + res);
+                Console.ReadKey();
+
+            }
+        }
+    }
+    
+
