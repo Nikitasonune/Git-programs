434f381454a1a7f2296aaa8c5a614ea22e9ee36e
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:06:14 -0700

    Exception Handling

---
+++ ExceptionHandling/Exception3_ArrayIndexOutOfBound/Exception3_ArrayIndexOutOfBound/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exception3_ArrayIndexOutOfBound
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.Array Index out of range (bound)
+             try
+             {
+                 int[] arr = new int[3];
+                 int i;
+                 Console.WriteLine("enter array element: ");
+
+                 for (i=0;i<=4;i++)
+                 {
+                     arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                 }
+             }
+             catch (DivideByZeroException e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             catch (Exception e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             Console.WriteLine("code reached till the end");
+             Console.ReadKey();
+        }
+    }
+}
