d1e8835a05f932adaa1f276ab8fe9fff41352e0d
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 11:59:13 -0700

    Assignment

---
+++ CsharpPrograms/BasicProgram/Assign_1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment1;
+namespace Assign_1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*  welcome obj = new welcome();
+              string res= obj.method2("Welcome Friends!");
+              string actual= "Have a nice day!";
+              string expected = res;
+              Assert.AreEqual(actual, expected);*/
+
+
+
+            Program obj = new Program();
+            int res = obj.addition(10,50);
+            int actual = 60;
+            int expected = res;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
---
+++ CsharpPrograms/BasicProgram/Assignment1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Program
+    {
+        //program 1
+         static void Main(string[] args)
+         {
+             welcome obj = new welcome();
+             string s1 = "Welcome Friends!";
+             string store = obj.method2(s1);
+             Console.WriteLine(store);
+             Console.ReadKey();
+         }
+     }
+    public class welcome
+    {
+        public string method2(string s2)
+        {
+            Console.WriteLine(s2);
+            string str = "Have a nice day!";
+            return str;
+
+        }
+    }
+}
