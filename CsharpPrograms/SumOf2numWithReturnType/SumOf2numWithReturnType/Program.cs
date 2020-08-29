7469a40f5c498f1726964f5e1e46f69534519352
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:03:04 -0700

    Sum of two number with return type method

---
+++ CsharpPrograms/SumOf2numWithReturnType/SumOf2numWithReturnType/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SumOf2numWithReturnType
+{
+    class Program
+    {
+        //3.Sum of two number(With argument and with return type)
+        static void Main(string[] args)
+        {
+            int num1, num2, res;
+            Console.WriteLine("enter number 1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number 2: ");
+            num2 = int.Parse(Console.ReadLine());
+            Program obj = new Program();
+            res = obj.addition(num1, num2);
+            Console.WriteLine("Sum of two numbers are: " + res);
+            Console.ReadKey();
+        }
+        public int addition(int a, int b)
+        {
+            int sum;
+            sum = a + b;
+            return sum;
+
+
+        }
+    }
+
+}
+
