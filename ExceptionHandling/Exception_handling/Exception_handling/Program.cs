8172793621fadad238d0dc6e72e96f1b3a5cec45
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:05:37 -0700

    Exception handling

---
+++ ExceptionHandling/Exception_handling/Exception_handling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             try
+             {
+                 Console.WriteLine("enter a number: ");
+                 int i = int.Parse(Console.ReadLine());
+                 Console.WriteLine("number is: " + i);
+             }
+             catch(OverflowException e)
+             {
+                 Console.WriteLine(e);
+             }
+             catch(Exception e)
+             {
+                 Console.WriteLine(e.Message);
+             }
+             Console.WriteLine("code reached till the end");
+             Console.ReadKey();
+
+        }
+    }
+}
