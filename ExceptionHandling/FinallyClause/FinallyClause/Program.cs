31d9ade42a621ba28735db38955117964d42b4c2
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:06:48 -0700

    Exception Finally clause

---
+++ ExceptionHandling/FinallyClause/FinallyClause/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FinallyClause
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4.The finally clause
+
+              decimal num, square;
+              try
+              {
+                  Console.WriteLine("enter number: ");
+                  num = Convert.ToDecimal(Console.ReadLine());
+                  square = num * num;
+                  Console.WriteLine("square is: " + square);
+              }
+              catch (Exception e)
+              {
+                  Console.WriteLine(e.ToString());
+              }
+              finally
+              {
+                  Console.WriteLine("finally executed");
+              }
+              Console.ReadKey();
+        }
+    }
+}
