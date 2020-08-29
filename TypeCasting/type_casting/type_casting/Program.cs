fdbd47588f1f0dc52ba9ef16456834cebe9d23a3
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:10:10 -0700

    type casting

---
+++ TypeCasting/type_casting/type_casting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace type_casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Data type conversion
+            int intvalue = 123;
+            long longvalue = intvalue;
+            Console.WriteLine("(long){0}={1}",intvalue, longvalue);
+            Console.ReadKey();
+
+        }
+    }
+}
