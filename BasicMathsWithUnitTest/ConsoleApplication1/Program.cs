20d4e1c5f289bce717f83fad7b20c10f38cf2b94
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 11:51:56 -0700

    Basic programs with unit Testing

---
+++ BasicMathsWithUnitTest/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class BasicMaths
+    {
+        public double Add(double num1,double num2)
+        {
+            return num1 + num2;
+        }
+        public double subtract(double num1,double num2)
+
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1,double num2)
+        {
+            return num1 / num2;
+        }    
+        public double multiply(double num1,double num2)
+        {
+            return num1 * num2;
+        }
+        static void Main(string[] args)
+        {
+            BasicMaths bm = new BasicMaths();
+            Console.WriteLine("enter 2num");
+            double n1 = Convert.ToInt32(Console.ReadLine());
+            double n2 = Convert.ToInt32(Console.ReadLine());
+            double res = bm.Add(n1, n2);
+            Console.WriteLine("Add=" + res);
+        }
+    }
+}
---
+++ BasicMathsWithUnitTest/basicmaths_23/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace basicmaths_23
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
