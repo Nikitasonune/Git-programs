dfb6884d6bc76dd9d072ca962d389e74d895e406
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:08:18 -0700

    Exception Handling

---
+++ ExceptionHandling/exceptionHandling2/exceptionHandling2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace exceptionHandling2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Program
+            try
+            {
+                Console.WriteLine("enter first number: ");
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter second number: ");
+                int j = int.Parse(Console.ReadLine());
+                int k = i / j;
+            }
+            catch(OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("code reached till the end");
+            Console.ReadKey();
+        }
+    }
+}
