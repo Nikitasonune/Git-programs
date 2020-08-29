2f81ba244c9cd2eccbeca8c7c0cbdbb65adeee41
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 11:58:15 -0700

    Assignment

---
+++ CsharpPrograms/Basic_C_Sharp/Basic_C_Sharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Basic_C_Sharp
+{
+    class Program
+    { 
+            //2.Program
+             static void Main(string[] args)
+             {
+                 string str;
+                 Console.WriteLine("Enter your name: ");
+                 str = Console.ReadLine();
+                 John(str);
+
+                 Console.ReadKey();
+             }
+             static void John(string str1)
+             {
+                 Console.WriteLine("welcome Friend: " + str1);
+                 Console.WriteLine("Have a nice day!");
+
+             }
+        }
+    }
