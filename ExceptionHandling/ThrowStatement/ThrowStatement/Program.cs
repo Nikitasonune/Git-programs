e5121ebe0c938aa8586b6b4ff2dca97b8a507d3a
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:07:41 -0700

    Exception Throw Statement

---
+++ ExceptionHandling/ThrowStatement/ThrowStatement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ThrowStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.Throw statement
+            int minute;
+            try
+            {
+                Console.WriteLine("enter minute: ");
+                minute = int.Parse(Console.ReadLine());
+                if (minute <= 1 || minute >= 60)
+                {
+                    throw new Exception(minute + " is not a valid minute");
+                }
+                Console.WriteLine("minute is " + minute);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
