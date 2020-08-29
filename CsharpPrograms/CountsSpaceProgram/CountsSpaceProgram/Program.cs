525cdb2ca031931b3000e806e46520193f09c793
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:00:26 -0700

    count space

---
+++ CsharpPrograms/CountsSpaceProgram/CountsSpaceProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CountsSpaceProgram
+{
+    class Program
+    {
+        //4.program to count spaces
+         static void Main(string[] args)
+         {
+             string str;
+             Console.WriteLine("Enter any statement: ");
+             str = Console.ReadLine();
+             space();
+             Console.ReadKey();
+         }
+         static int space()
+         {
+             int sp=0;
+
+         }
+    }
+}
