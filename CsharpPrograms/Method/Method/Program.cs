a060d396e9b47ea5576be7b0d9eff81f045e43ed
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:01:37 -0700

    Method Programs

---
+++ CsharpPrograms/Method/Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Example();
+            Console.ReadKey();
+        }
+        void Example()
+        {
+            Console.WriteLine("Hello Nikita Here");
+
+        }
+    }
+}
