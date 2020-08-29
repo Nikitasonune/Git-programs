c854080f9231c2d9accc9086cc2088427ee1d0be
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 11:50:35 -0700

    Find space in string

---
+++ AssignmentFindSpaceInString/Assignment_prog4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_prog4
+{
+    public class Program
+    {
+        //4.find spaces in string
+        static void Main(string[] args)
+        {
+            string str;
+            int i;
+            Console.WriteLine("enter any string: ");
+            str = Console.ReadLine();
+            Program obj = new Program();
+
+            i = obj.function(str);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+        public int function(string a)
+        {
+            int count = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ AssignmentFindSpaceInString/space/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_prog4;
+namespace space
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program obj = new Program();
+            int res = obj.function("my name m");
+            int actual = 2;
+            int expected = res;
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}
