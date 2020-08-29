6d39842d75914072620f74a1b7d2c1a08b6782f2
Nikitasonune <nikitasonune@gmail.com>
2020-08-29 12:01:03 -0700

    Greatest Number

---
+++ CsharpPrograms/GreatestNumber/ConsoleApplication3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter num1");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter num2");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter num3");
+            int num3 = int.Parse(Console.ReadLine());
+
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("num1 is greatest");
+            }
+            else if (num2 > num3 && num2 > num1)
+            {
+                Console.WriteLine("num2 is greatest");
+            }
+            else
+            {
+                Console.WriteLine("num3 is greatest");
+            }
+            Console.ReadKey();
+
+
+
+        }
+            
+
+           
+
+    }
+}
